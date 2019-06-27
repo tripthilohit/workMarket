137aeec2e11cd2033587b7988c2cfefd23c61bf8
tripthilohit <38136831+tripthilohit@users.noreply.github.com>
2019-06-27 15:30:06 -0400

    Update compassTest.js

--- tests/compassTest.js
+++ tests/compassTest.js
@@ -1,9 +1,21 @@
-var allLocation = []
-var uniqueLocation = []
-var login = function(driver)
+
+/*globals
+homeURL: 'https://www.compass.com/',
+  compassEmail: 'compass9012@gmail.com',
+  compassPassword: 'Takehome1234567890',
+  nycSalesListURL: 'https://www.compass.com/search/sales/nyc',
+  listingCount: 5
+  */
+
+
+
+var allLocation = []	//array to store all location values
+var uniqueLocationIndex = [] //array to store all the indexes of top unique locations
+
+var login = function(driver)		//This module logins to the application 
 {
 	driver
-	.url("https://www.compass.com/") //add to global file
+	.url(driver.globals.userNames.homeURL) //variables are pulled from global file
 	.windowMaximize("current")
 	.waitForElementVisible('body', 2000)
 	.verify.title('Real Estate, Homes for Sale & Apartments for Rent | Compass')
@@ -11,28 +23,26 @@ var login = function(driver)
 	.click("button[class='uc-corpNav-button uc-corpNav-menuItem textIntent-caption1 uc-corpNav-authBtn uc-corpNav-loginBtn']")
 	.waitForElementVisible("input[name='email']",2000)
 	.click("input[name='email']")
-	.setValue("input[name='email']","compass9012@gmail.com")   //add to global file
+	.setValue("input[name='email']",driver.globals.userNames.compassEmail)  
 	.click("input[name='password']")
-	.setValue("input[name='password']","Takehome1234567890") //add to global file
+	.setValue("input[name='password']",driver.globals.userNames.compassPassword) 
 	.click("button[class='uc-authenticationForm-submit cx-solidBtn cx-solidBtn--brand']")
 	.pause(1000)		//remove pause later
 }
 
-var getLocation = function(driver)
+var getLocation = function(driver)						//This module pushes all the location (in descending order of price) into an array
 {
-	
 	driver
-	.url("https://www.compass.com/search/sales/nyc") //add to global file
+	.url(driver.globals.userNames.nycSalesListURL) 
 	.waitForElementVisible('body', 2000)
-	.verify.title('Compass Search')
 	.click("button[data-tn='lolMenu-button-tableView']")
 	.click("button[data-tn='lolMenu-button-supportView-map']")
 	.click("dt[title='Locations']")
-	//.waitForElementVisible("div[class='uc-searchBarLocations-neighborhood']",2000)
 	.useXpath()
 	.click("(//div[@class='searchFilter-section-container ']/div)[3]/label[1]")
 	.pause(2000)
 	.click("(//div[@class='ag-header-cell ag-header-cell-sortable ag-header-cell--number']/div/span[4])[1]")
+	.pause(1000)
 	.click("(//div[@class='ag-header-cell ag-header-cell-sortable ag-header-cell--number']/div/span[2])[1]")
 	.pause(2000)
 	function searchCriteriaResults(elements) {
@@ -45,69 +55,52 @@ var getLocation = function(driver)
 
       });
     }
-    driver.elements('xpath', "//div[@colid='listing.location.neighborhood']", searchCriteriaResults)
-    
+    driver.elements('xpath', "//div[@colid='listing.location.neighborhood']", searchCriteriaResults)   
 }
-var printLocation = function(driver)
-{
 
-	for(var i=0;i<allLocation.length;i++)
-		console.log(allLocation[i])
-}
-var getUniqueLocationIndex = function(driver)
+var getUniqueLocationIndex = function(driver)		//This module pushes index of the top most expensive listings with unique location into an array
 {	
-	var visited = []
 
+	var visited = []
 	for(var i=0;i<allLocation.length;i++)
 		visited[i]= false
-
 	for(var i=1;i<allLocation.length;i++)
 	{
 		if(!visited[i])
 		{
-		for(var j=i+1;j<allLocation.length;j++)
-		{
-			
-			
+			for(var j=i+1;j<allLocation.length;j++)
+			{
 				if(allLocation[i]==allLocation[j])
 				{	
-
-					visited[j]=true
-					
+					visited[j]=true	
 				}
-			
-		}}
+			}	
+		}
 		if(!visited[i])
-			uniqueLocation.push(i)
+			uniqueLocationIndex.push(i)
 	}
-console.log(uniqueLocation)
 }
 
-var returnURL = function(driver)
+var printURLonConsole = function(driver)			//This module prints the 'n' URL of the sales listing
 {
-
 	driver
 	.useXpath()
-	for(var i=0;i<5;i++)
+	for(var i=0;i<driver.globals.userNames.listingCount;i++)
 	{
-		console.log(uniqueLocation[i])
-		var k = uniqueLocation[i]
-		driver.pause(2000)
-	driver.click("(//div[@colid='listing.location.address'])["+(k+1)+"]")
-	driver.pause(3000)
-	//driver.waitForElementVisible("//a[@class='uc-listingNavigation-externalLink uc-listingNavigation-btn cx-nakedBtn cx-nakedBtn--sm']",3000)
-	driver.getAttribute("//a[@class='uc-listingNavigation-externalLink uc-listingNavigation-btn cx-nakedBtn cx-nakedBtn--sm']","href",function(url)
-	{console.log(url.value)})
+		//console.log(uniqueLocationIndex[i])
+		var k = uniqueLocationIndex[i]
+		driver.click("(//div[@colid='listing.location.address'])["+(k+1)+"]")
+		driver.pause(1000)
+		driver.getAttribute("//a[@class='uc-listingNavigation-externalLink uc-listingNavigation-btn cx-nakedBtn cx-nakedBtn--sm']","href",function(url)
+		{	console.log("\n")
+			console.log(url.value)})
+	}
 }
-}
-//driver.elements('xpath', "//div[@class='ag-row ag-row-no-focus ag-row-no-animation ag-row-level-0 ag-row-even']", searchCriteriaResults)
-////div[@colid='listing.location.neighborhood']
-////div[@colid='listing.price.lastKnown']
+
 module.exports = {
     login: login,
     getLocation: getLocation,
-    printLocation:printLocation,
     getUniqueLocationIndex:getUniqueLocationIndex,
-    returnURL: returnURL
+    printURLonConsole: printURLonConsole
 
 }
